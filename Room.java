9f3cbe7ad0ec51fa2e8850e98dbef1d85e4a5fb6
swamysanjay <swamysanjay98@gmail.com>
2017-12-03 16:20:57 -0500

    Add files via upload

--- Room.java
+++ Room.java
@@ -2,16 +2,26 @@ import java.util.List;
 
 public class Room {
     protected List <Player> people;
+    protected List <Thing> things;
     protected int rows;
     protected int columns;
+    protected boolean [] directions = new boolean[False, False, False, False];
 
-    public Room (List <Player> people, int rows, int columns) {
+
+    public Room (List <Player> people, List <Thing> things, int rows, int columns, boolean[] directions) {
         this.people = people;
+        this.things = things;
         this.rows = rows;
         this.columns = columns;
+        this.directions = directions;
+
     }
     public List <Player> getPlayers() {
         return people;
+    }
+
+    public List <Thing> getThings () {
+        return things;
     }
 
     public int getRows () {
@@ -22,10 +32,20 @@ public class Room {
         return columns;
     }
 
+    public boolean[] getDirections() {
+        return directions;
+    }
+
     public void addPlayer (Player p) {
         this.people.add(p);
     }
     public void removePlayer (Player p) {
         this.people.remove (p);
     }
+
+    public void addThings (Thing t) {
+        this.things.add (t);
+    }
+
+
 }
