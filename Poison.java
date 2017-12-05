c173a5a609674c84ec8d2fe72286acd9fcb02e25
Ashlee Foureyes <ashleefoureyes@gmail.com>
2017-12-05 12:56:11 -0500

    Poison added
    Poison

---
+++ Poison.java
@@ -0,0 +1,16 @@
+/**
+ * Created by ashleefoureyes on 2017-12-05.
+ */
+public class Poison extends Thing{
+
+    public Poison(String name, int healthvalue, double x, double y){
+        super(name, healthvalue, x, y);
+        this.name = "Hamburger";
+        this.healthvalue = -15;
+    }
+
+    public void interact(){
+        System.out.println("You ate poison! 15 HP have been subtracted from your health!");
+    }
+
+}
