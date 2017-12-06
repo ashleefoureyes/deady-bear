54044259e1328770c2ad959048525a7a7d982f7f
Rishi Parameswaran <33700721+LuusuRishi@users.noreply.github.com>
2017-12-05 21:04:25 -0500

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,14 +1,11 @@
 public class Food extends Thing{
 
-    public Food(String name, int healthvalue, double x, double y){
-        super(name, healthvalue, x, y);
+    public Food(String name, String description, int healthvalue, double x, double y) {
+        super(name, description, healthvalue, x, y);
         this.name = "Hamburger";
+        this.description = "You obtained a hamburger! 25HP added to your health!";
         this.healthvalue = 25;
     }
 
-    public void interact(Player p){
-        System.out.println("You obtained a hamburger! 25HP added to your health!");
-        p.setHealth(this.healthvalue + p.getHealth());
-    }
-
 }
+
